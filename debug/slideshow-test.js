3f55453e786c9fa148ac8b7777e62c2b112e07d1
Li Ge <lge@ucsd.edu>
2014-12-25 12:43:50 -0800

    add ldl api and mock data api

--- debug/slideshow-test.js
+++ debug/slideshow-test.js
@@ -1,12 +1,3 @@
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
 var w = 2*Math.PI/10000;
 var slides = [
   new Slide('slider-0', function(t) {
@@ -20,3 +11,16 @@ var slides = [
   })
 ],
     ss = new SlideShow(document.body, slides);
+
+
+LDLAPI.getAnnualUserData(null, function(err, data) {
+  if (err) {
+    console.log("err = ", err);
+    return;
+  }
+
+  slides.forEach(function(slide) {
+    slide.context = data.ret;
+  });
+  ss.start();
+});
