86212d864a0ba92ca95532da2a6c97dfd1f9ff92
Li Ge <lge@ucsd.edu>
2014-12-25 11:51:44 -0800

    support slide animation

--- debug/slideshow-test.js
+++ debug/slideshow-test.js
@@ -1,2 +1,22 @@
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
 
-var ss = new SlideShow();
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+var w = 2*Math.PI/10000;
+var slides = [
+  new Slide('slider-0', function(t) {
+    this.domEl.style.backgroundColor = rgbToHex(0, 0, Math.floor(128*(1+Math.sin(2*w*t))));
+  }),
+  new Slide('slider-1', function(t) {
+    this.domEl.style.backgroundColor = rgbToHex(0, Math.floor(128*(1+Math.sin(2*w*t))), 0);
+  }),
+  new Slide('slider-2', function(t) {
+    this.domEl.style.backgroundColor = rgbToHex(Math.floor(128*(1+Math.sin(2*w*t))), 0, 0);
+  })
+],
+    ss = new SlideShow(document.body, slides);
