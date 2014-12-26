f8c7c0481cf034aacaf9eb4adf472a4f0a405bfc
Li Ge <lge@ucsd.edu>
2014-12-25 17:26:01 -0800

    add hooks onEnter,onCreate,onExit,onFrame to Slide; move container instead of each slides

---
+++ debug/animation-hooks/main.js
@@ -0,0 +1,50 @@
+var T = 10000,
+    updateColorComponent = (function() {
+      var w = 2*Math.PI/T;
+      return function(t) {
+        var zeroToOne = 0.5*(1+Math.cos(2*w*t));
+        return Math.floor(100 + (256-100)*zeroToOne);
+      };
+    })(),
+    slides = [0, 1, 2].map(function(i) {
+      var id = 'slide-' + i,
+          colorComponents = [0, 0, 0],
+          createFn = function() {
+            var h1 = document.createElement('h1');
+            h1.textContent = '' + i;
+            this.domEl.appendChild(h1);
+          },
+          frameFn = function(t) {
+            var comps = colorComponents.slice();
+            comps[i] = updateColorComponent(t);
+            this.domEl.style.backgroundColor = rgbToHex.apply(null, comps);
+          },
+          enterFn = function() {
+            var h2 = document.createElement('h2'),
+                later = function() {
+                  this.domEl.removeChild(h2);
+                };
+
+            debugger;
+            h2.textContent = 'entered slide ' + i;
+            this.domEl.appendChild(h2);
+            setTimeout(later.bind(this), 2000);
+          },
+          exitFn = function() {
+            var comps = colorComponents.slice();
+            comps[i] = 255;
+            this.domEl.style.backgroundColor = rgbToHex.apply(null, comps);
+          };
+
+      return new Slide({
+        domEl: id,
+        onCreate: createFn,
+        onEnter: enterFn,
+        onExit: exitFn,
+        onFrame: frameFn
+      });
+
+    }),
+    ss = new SlideShow(document.body, slides);
+
+ss.start();
--- debug/swipe-events/main.js
+++ debug/swipe-events/main.js
@@ -1,6 +1,7 @@
-var slides = ['slide-0', 'slide-1', 'slide-2'].map(function(id) {
-  return new Slide(id);
-}),
+var slides = ['slide-0', 'slide-1', 'slide-2']
+      .map(function(id) {
+        return new Slide({ domEl: id });
+      }),
     ss = new SlideShow(document.body, slides);
 
 ss.start();
