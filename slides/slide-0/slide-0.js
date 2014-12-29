f1eba6d5fa3a95e9971f35798debdf45947bd57d
Li Ge <lge@ucsd.edu>
2014-12-29 13:07:48 -0800

    star animation

--- slides/slide-0/slide-0.js
+++ slides/slide-0/slide-0.js
@@ -1,13 +1,66 @@
 (function(app) {
-  var slide = app.addSlide('slide-0', {
+  var $ = app.$,
+      compile = app.compile,
+      starSource = $('#slide-0-template-star').html(),
+      starTemplate = compile(starSource),
+      starSize = 32,
+      starCenters = [
+        { x:249, y: 316 },
+        { x:285, y: 309 },
+        { x:321, y: 302 },
+        { x:357, y: 295 }
+      ];
+
+  function createStar(id, point, size) {
+    var data = {
+      id: id,
+      x: point.x,
+      y: point.y,
+      size: size,
+      tx: -size/2,
+      ty: -size/2
+    };
+    return starTemplate(data);
+  }
+
+  function presentStar(index, duration, delay) {
+    return setTimeout(function() {
+      $('#slide-0-star-' + index)
+        .css('display', '')
+        .velocity({
+          width: starSize,
+          height: starSize,
+          translateX: -starSize/2,
+          translateY: -starSize/2
+        }, {
+          duration: duration
+        });
+      return duration;
+    }, delay);
+  }
+
+  app.addSlide('slide-0', {
     onCreate: function() {
-      console.log('slide-0.context', this.context);
+      this.starContainer = $('#slide-0-stars', this.domEl)[0];
+      this.animations = [];
     },
     onEnter: function() {
+      var duration = this.context.starAnimationDuration || 500;
+      starCenters.forEach(function(p, i) {
+        var html = createStar('slide-0-star-'+i, p, starSize*10);
+        this.starContainer.innerHTML += html;
+      }, this);
 
+      this.animations.push(presentStar(0, duration, 0*duration));
+      this.animations.push(presentStar(1, duration, 1*duration));
+      this.animations.push(presentStar(2, duration, 2*duration));
+      this.animations.push(presentStar(3, duration, 3*duration));
     },
     onExit: function() {
-
+      this.animations.forEach(function(id) {
+        clearTimeout(id);
+      });
+      this.starContainer.innerHTML = '';
     }
   });
 })(app);
