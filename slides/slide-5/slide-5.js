5e5a876053f393314e60d277f15b15320ca40d58
Li Ge <lge@ucsd.edu>
2014-12-29 21:47:39 -0800

    slide-5 better than animation

--- slides/slide-5/slide-5.js
+++ slides/slide-5/slide-5.js
@@ -1,7 +1,8 @@
 (function(app) {
   var compile = app.compile,
       $ = app.$,
-      text1Template = compile($('#slide-5-template-text-1').html());
+      text1Template = compile($('#slide-5-template-text-1').html()),
+      rankCircleTemplate = compile($('#slide-5-template-rank-circle').html());
 
   function animateTextNumber(el, dest, duration) {
     var startTime = Date.now(),
@@ -45,27 +46,65 @@
     };
   }
 
+
+  function animateRankCircle($el, x, y, destScale, delay, duration) {
+    $el
+      .velocity({
+        translateX: x,
+        translateY: y,
+        scale: 0.0
+      }, {
+        delay: delay,
+        duration: 0
+      })
+      .velocity({
+        scale: destScale
+      }, {
+        duration: duration
+      });
+
+    return {
+      stop: function() {
+        $el.velocity('stop', true);
+        $el[0].scale = 0.0;
+        $el[0].translateX = x;
+        $el[0].translateY = y;
+      }
+    };
+  }
+
   app.addSlide('slide-5', {
     onCreate: function() {
       var svg = $('svg', this.domEl)[0];
       svg.innerHTML += text1Template(this.context);
+      svg.innerHTML += rankCircleTemplate(this.context);
 
       this.totalStepTextEl = $('#slide-5-total-steps')[0];
       this.avgStepTextEl = $('#slide-5-avg-steps')[0];
 
       this.$clipRect = $('#slide-5-bright-map-clip rect');
 
+      this.$rankCircle = $('#slide-5-rank-circle');
+
       this.animationHandles = [];
     },
     onEnter: function() {
-      var duration = this.context.stepsAccumulateAnimationDuration || 2000,
+      var stepAccDuration = this.context.stepsAccumulateAnimationDuration || 2000,
+          rankCircleDuration = this.context.rankCircleAnimationDuration || 200,
           destWidth = 800,
           totalSteps = this.context.userData.accumulativesteps,
           avgSteps = this.context.userData.averagesteps;
 
-      this.animationHandles.push(animateTextNumber(this.totalStepTextEl, totalSteps, duration));
-      this.animationHandles.push(animateTextNumber(this.avgStepTextEl, avgSteps, duration));
-      this.animationHandles.push(animateRectWidth(this.$clipRect, destWidth, duration));
+      this.animationHandles.push(animateTextNumber(this.totalStepTextEl, totalSteps, stepAccDuration));
+      this.animationHandles.push(animateTextNumber(this.avgStepTextEl, avgSteps, stepAccDuration));
+      this.animationHandles.push(animateRectWidth(this.$clipRect, destWidth, stepAccDuration));
+      this.animationHandles.push(animateRankCircle(
+        this.$rankCircle,
+        this.context.circleCenterX,
+        this.context.circleCenterY,
+        1.0,
+        stepAccDuration,
+        rankCircleDuration));
     },
     onExit: function() {
       this.animationHandles.forEach(function(handle) {
