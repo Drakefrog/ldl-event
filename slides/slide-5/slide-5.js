07edef7d20ce2df68b22c481ff71a0c041303654
Li Ge <lge@ucsd.edu>
2014-12-29 20:32:12 -0800

    steps acc animation

--- slides/slide-5/slide-5.js
+++ slides/slide-5/slide-5.js
@@ -30,6 +30,21 @@
     };
   }
 
+  function animateRectWidth($rect, destW, duration) {
+    $rect.velocity({
+      width: destW
+    }, {
+      duration: duration
+    });
+
+    return {
+      stop: function() {
+        $rect[0].width = 0;
+        $rect.velocity('stop');
+      }
+    };
+  }
+
   app.addSlide('slide-5', {
     onCreate: function() {
       var svg = $('svg', this.domEl)[0];
@@ -37,15 +52,20 @@
 
       this.totalStepTextEl = $('#slide-5-total-steps')[0];
       this.avgStepTextEl = $('#slide-5-avg-steps')[0];
+
+      this.$clipRect = $('#slide-5-bright-map-clip rect');
+
       this.animationHandles = [];
     },
     onEnter: function() {
-      var duration = this.context.stepCountAnimationDuration || 500,
+      var duration = this.context.stepsAccumulateAnimationDuration || 2000,
+          destWidth = 800,
           totalSteps = this.context.userData.accumulativesteps,
           avgSteps = this.context.userData.averagesteps;
 
       this.animationHandles.push(animateTextNumber(this.totalStepTextEl, totalSteps, duration));
       this.animationHandles.push(animateTextNumber(this.avgStepTextEl, avgSteps, duration));
+      this.animationHandles.push(animateRectWidth(this.$clipRect, destWidth, duration));
     },
     onExit: function() {
       this.animationHandles.forEach(function(handle) {
